93526fcaa0cf62b96e7ad721a93d0fb0bf2eacfe
Alberelloo <IvanovDaniil3@yandex.ru>
2022-02-12 01:21:26 +0300

    homework_1 (#1-7)

---
+++ task_1.py
@@ -0,0 +1,19 @@
+# Поработайте с переменными, создайте несколько, выведите на экран. Запросите у пользователя некоторые числа
+# и строки и сохраните в переменные, затем выведите на экран.
+
+number = 25
+str = 'Мама, я кодер'
+
+print(number)
+print(str)
+
+input_number_1 = int(input('Введите число:'))
+input_number_2 = int(input('Введите число:'))
+input_str_1 = input('Введите текст:')
+input_str_2 = input('Введите текст:')
+
+print('-' * 50)
+print(input_number_1)
+print(input_number_2)
+print(input_str_1)
+print(input_str_2)
